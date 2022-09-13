22a81f1cd6957a409eb5a3cf37ad0755086f5198
Andrey Rybalkin <92117385+andreysavie@users.noreply.github.com>
2022-09-13 21:49:32 +0300

    files added

---
+++ SwiftUI-TestApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI-TestApp
+//
+//  Created by Андрей Рыбалкин on 13.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-TestApp/SwiftUI_TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TestAppApp.swift
+//  SwiftUI-TestApp
+//
+//  Created by Андрей Рыбалкин on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
